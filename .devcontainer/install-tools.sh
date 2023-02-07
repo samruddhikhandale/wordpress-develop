8f27bd9993dc3e4a082d6d409f1b7bc75845068a
Samruddhi Khandale <skhandale@microsoft.com>
2023-02-07 22:28:30 +0000

    nit

--- .devcontainer/install-tools.sh
+++ .devcontainer/install-tools.sh
@@ -7,4 +7,5 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 sudo chmod +x wp-cli.phar
 sudo mv wp-cli.phar /usr/local/bin/wp
 
+# Copy the welcome message
 sudo cp .devcontainer/welcome-message.txt /usr/local/etc/vscode-dev-containers/first-run-notice.txt
