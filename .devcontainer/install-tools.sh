37273ccc941c3cfe95d75e3ba786030fcb4280cd
Samruddhi Khandale <skhandale@microsoft.com>
2023-02-07 01:03:34 +0000

    draft

---
+++ .devcontainer/install-tools.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+set -eux
+
+echo "Installing wp..."
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+sudo chmod +x wp-cli.phar
+sudo mv wp-cli.phar /usr/local/bin/wp
